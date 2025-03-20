4534f81c1818d078703e99d9169e54172346ce63
JRGCH <jroberto.granada@gmail.com>
2025-03-20 12:05:01 -0500

    Cambio en la importacion de systeminformation

--- src/system-information/system-information.service.ts
+++ src/system-information/system-information.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import si from "systeminformation";
+import * as si from "systeminformation";
 
 export interface SystemSerialInfo {
     systemInfoSerial: string;
