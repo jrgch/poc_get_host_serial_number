d3f9051462c205343c45d0a2519c04e19e262754
JRGCH <jroberto.granada@gmail.com>
2025-03-20 01:44:42 -0500

    Reestructuracion del servicio

--- src/system-information/system-information.controller.ts
+++ src/system-information/system-information.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { SystemInformationService } from './system-information.service';
+import { SystemInformationService, SystemSerialInfo } from './system-information.service';
 
 @Controller('system-information')
 export class SystemInformationController {
   constructor(private readonly systemInformationService: SystemInformationService) { }
 
   @Get()
-  async getSerialNumber(): Promise<string | undefined> {
+  async getSerialNumber(): Promise<SystemSerialInfo | undefined> {
     return await this.systemInformationService.getSerialNumber();
   }
 }
--- src/system-information/system-information.service.ts
+++ src/system-information/system-information.service.ts
@@ -1,32 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import si from "systeminformation";
-const six = require("systeminformation");
+
+export interface SystemSerialInfo {
+    systemInfoSerial: string;
+    sysbiosSerial: string;
+    sychasisSerial: string;
+    baseboardInfoSerial: string;
+}
 
 @Injectable()
 export class SystemInformationService {
 
-    async getSerialNumber(): Promise<string|undefined> {
+    async getSerialNumber(): Promise<SystemSerialInfo | undefined> {
         try {
             // Obtener información del sistema
-            const systemInfo = await six.system();
-            const sysbis = await six.bios();
-            const sychasis = await six.chassis();
-            const baseboardInfo = await six.baseboard();
+            const systemInfo = await si.system();
+            const sysbios = await si.bios();
+            const sychasis = await si.chassis();
+            const baseboardInfo = await si.baseboard();
 
-            console.log(systemInfo)
-            console.log(sysbis)
-            console.log(sychasis)
-            console.log(baseboardInfo)
+            const resp = {
+                systemInfoSerial: systemInfo?.serial ?? "",
+                sysbiosSerial: sysbios?.serial ?? "",
+                sychasisSerial: sychasis?.serial ?? "",
+                baseboardInfoSerial: baseboardInfo?.serial ?? ""
+            }
 
-
-
-            //const serial1 = systemInfo.serial
-            //const serial2 = baseboardInfo.serial;
-
-            //console.log("Serial del Sistema:", serial1 || "No disponible");
-            //console.log("Serial de la Placa Base:", serial2 || "No disponible");
-
-            return "xxx"//serial1 + "-" + serial2;
+            return resp;
 
         } catch (error) {
             console.error("Error obteniendo el serial:", error);
